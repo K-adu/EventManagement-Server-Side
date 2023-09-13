2afc2b5501741d5f39e28d5213919eed13faed13
K-adu <manishbaral112@gmail.com>
2023-09-13 15:53:07 +0545

    chores:codes

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post, Res, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Post, Res, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDTO } from 'src/user/dto/create-user.dto';
 import { LoginUserDto } from './dto/login-user.dto';
@@ -20,26 +20,37 @@ export class AuthController {
     res
       .cookie('access_token', access_token, {
         //httpOnly: true,
-        secure: false,
+        //secure: false,
       })
       .send({ status: 'ok' });
   }
   //@UseGuards(AuthGuard)
-  @Post('/logout')
+  // @Post('/logout')
+  // async logOut(@Res({ passthrough: true }) res) {
+  //   console.log('logout');
+  //   console.log(res);
+  //   try {
+  //     res
+
+  //       .cookie('access_token', '', {
+  //         // httpOnly: false,
+  //         secure: false,
+  //         expires: new Date('Thu, 01 Jan 1970 00:00:00 GMT'), // Set expiration to the past
+  //       })
+  //       .send({ status: 'ok' });
+  //   } catch (e) {
+  //     console.log(e);
+  //   }
+  // }
+  @Get('/logout')
   async logOut(@Res({ passthrough: true }) res) {
     console.log('logout');
-    console.log(res.cookie);
     try {
-      res
-
-        .cookie('access_token', '', {
-          // httpOnly: false,
-          secure: false,
-          //expires: new Date('Thu, 01 Jan 1970 00:00:00 GMT'), // Set expiration to the past
-        })
-        .send({ status: 'ok' });
+      res.clearCookie('access_token', { httpOnly: false, secure: false }); // Clear the 'access_token' cookie
+      res.send({ status: 'ok' });
     } catch (e) {
       console.log(e);
+      res.status(500).send({ error: 'Internal Server Error' }); // Handle errors gracefully
     }
   }
 }
